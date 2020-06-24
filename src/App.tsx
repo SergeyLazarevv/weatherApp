ac172568f42b996f1eeb99e187c02f0558b7a149
Sergey <serg_lazarev13@mail.ru>
2020-06-25 02:02:40 +0400

    25.06.20

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Samara,ru&APPID=0cb0672cbaa9d7e3d59356e1bdbb7a31")
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => {
+	console.log(err);
+});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src='sun.png' className='logo' />
+        <ul className='menu'>
+          <li>About</li>
+          <li>News</li>
+          <li>Contacts</li>
+        </ul>
       </header>
     </div>
   );
