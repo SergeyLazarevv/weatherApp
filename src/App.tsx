cc8bb35a7b62343b8d69e2af47870643d12b61f4
Sergey <serg_lazarev13@mail.ru>
2020-07-29 00:19:59 +0400

    add input and search logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 import { Header } from './header';
 
-function App() {
-fetch("http://api.openweathermap.org/data/2.5/weather?q=Samara,ru&APPID=0cb0672cbaa9d7e3d59356e1bdbb7a31")
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(err => {
-	console.log(err);
-});
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+interface IMyComponentState {
+  sity: string
+}
+
+class App extends React.Component <any,IMyComponentState>{
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      sity: ""
+    }
+  }
+  getData = (event: any) => {
+    event.preventDefault();
+    let sity: string = this.state.sity;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${sity},ru&APPID=0cb0672cbaa9d7e3d59356e1bdbb7a31`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .then(data=>this.setState({sity: ''}))
+    .catch(err => {
+	  console.log(err);
+    });
+  }
+  setData = (event: any) => {
+    this.setState({sity: event.target.value})
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <form className="input-wrap" onSubmit={this.getData} >
+          <input className="input" type="text" placeholder="Enter sity" onChange={this.setData} value={this.state.sity} />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
