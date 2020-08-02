357bb20d6632e321b3fd65616e883eaeb4d1dac4
Sergey <serg_lazarev13@mail.ru>
2020-08-02 23:55:31 +0400

    add input refs and same animation change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import background from './img/background.svg';
 import './App.css';
 import Header from './header';
 import WeatherInfo from './WeatherInfo'
-
+//create interface for state
 interface AppComponentState {
   sity: string,
   infoVisible: boolean,
@@ -11,8 +11,11 @@ interface AppComponentState {
 }
 
 class App extends React.Component <any,AppComponentState>{
+  private inputRef: React.RefObject<HTMLInputElement>;
   constructor(props: any) {
     super(props)
+    //create ref for input field
+    this.inputRef = React.createRef();
     this.state = {
       sity: "",
       infoVisible: false,
@@ -33,6 +36,7 @@ class App extends React.Component <any,AppComponentState>{
   getData = (event: any) => {
     event.preventDefault();
     let sity: string = this.state.sity;
+    //get weather info
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${sity},ru&APPID=0cb0672cbaa9d7e3d59356e1bdbb7a31`)
     .then(res => res.json())
     .then((data)=> {
@@ -40,9 +44,14 @@ class App extends React.Component <any,AppComponentState>{
       console.log(this.state.data)
       return data
     })
+     //set data to state for using in child component
     .then(data => this.setState({data}))
+    //clear input field
     .then(data=>this.setState({sity: ''}))
+    //ifovisible for child information component rendering 
     .then(data=>this.setState({infoVisible: true}))
+    //remove focus from input and trasform them in close button
+    .then(data=>this.inputRef.current?.blur())
     .catch(err => {
       console.log(err);
     });
@@ -50,15 +59,20 @@ class App extends React.Component <any,AppComponentState>{
   setData = (event: any) => {
     this.setState({sity: event.target.value})
   }
+  newSearch = () => {
+    this.setState({infoVisible: false})
+  }
   render() {
     console.log('render!!!')
     return (
       <div className="app">
         <Header />
         <form className={this.state.infoVisible ? "input-wrap toTop" : "input-wrap"} onSubmit={this.getData} >
-          <input className={this.state.infoVisible ? "input input-close" : "input"} type="text" 
+          <input className={this.state.infoVisible ? "input input-close input-hover" : "input"} type="text" 
                   placeholder={this.state.infoVisible ? "X" : "Enter sity"} 
-                  onChange={this.setData} value={this.state.sity} />
+                  onChange={this.setData} value={this.state.sity}
+                  ref={this.inputRef} 
+                  onClick={this.newSearch} />
         </form>
         {this.state.infoVisible ? <WeatherInfo weatherInfo={this.state.data} /> : ''}
       </div>
