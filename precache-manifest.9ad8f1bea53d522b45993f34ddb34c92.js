e3258303f76ffbc0203aaf4fcdc3eeed2d06ddf1
Sergey <serg_lazarev13@mail.ru>
2020-08-03 01:08:22 +0400

    Updates

---
+++ precache-manifest.9ad8f1bea53d522b45993f34ddb34c92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f181ed1fae5a62de55084b09f7453008",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "c63fe267ea52a0d46d09",
+    "url": "/weatherApp/static/css/main.bfa044f6.chunk.css"
+  },
+  {
+    "revision": "912677aea47e180d081b",
+    "url": "/weatherApp/static/js/2.24279e79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.24279e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63fe267ea52a0d46d09",
+    "url": "/weatherApp/static/js/main.ec5b4aea.chunk.js"
+  },
+  {
+    "revision": "8ed9b36db091de228818",
+    "url": "/weatherApp/static/js/runtime-main.6df30674.js"
+  },
+  {
+    "revision": "176c1a2c53a39b5fda39e0ecd0b67d09",
+    "url": "/weatherApp/static/media/background.176c1a2c.svg"
+  }
+]);
